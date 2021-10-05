fd3664b0c90718523791062ee8b154c0162fbd56
dracero <diego.racero@gmail.com>
2021-10-05 17:09:02 -0300

    mejora

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -1,3 +1,5 @@
+import intents from "./intents";
+
 // ActionProvider starter code
 class ActionProvider {
   constructor(createChatBotMessage, setStateFunc) {
@@ -5,38 +7,6 @@ class ActionProvider {
     this.setState = setStateFunc;
   }
   greet(respuesta) {
-    const intents = {
-      mensajes: "Ponerse en contacto",
-      cambios: "Pagina de cátedra",
-      apuntes:
-        "Es necesario cambiar a FIUBA la institución en tu perfil. Luego",
-      ejercicios: "La Guía de problemas",
-      examenes: "Tanto la constancia de parcial como la de integrador",
-      aula_virtual: "En todos los casos el aula virtual",
-      preguntas: "Es necesario",
-      notas: "se forma",
-      coordinadora:
-        "la coordinación se encarga de la gestión de la asignatura.",
-      mail: "El mail es",
-      cursos: "tenés que ver el link",
-      sp: "Si sos recursantes",
-      informacion: "En la página de cátedra",
-      recursante: "Sos recursante si ",
-      inscripcion_final:
-        "Por una cuestión organizativa sólo podrás inscribirte en una fecha a la vez.",
-      examen_sinpresent:
-        "Si estás inscripto a un final y no venís no perdés una oportunidad.",
-      cert_exam: "Los certificados de examen se bajan del link de más abajo.",
-      ver_siu: "Tenés que verificar tu historia académica en el SIU-Guaraní.",
-      insc_fisica:
-        "No es posible anotarse en ninguna materia del departamento fuera de término.",
-      anot_nocorrel:
-        "Al anotarte en una materia, si no te desinscribís durante el período de desinscripción indicado en la pregunta anterior, perderás la cursada vigente y las chances que te quedan.",
-      hor_dep:
-        "Los horarios de las asginaturas están en la pagina del departamento.",
-      inscripcion:
-        "Te tenés que inscribir dentro del período habilitado a través del SIU."
-    };
     const traits = {
       no: "Enviar un mail.",
       si: "Llamar por teléfono.",
@@ -85,9 +55,8 @@ class ActionProvider {
       var key = [];
       for (var k in respuesta.entities) key.push(k);
       var confent = respuesta.entities[k][0].confidence; //nivel de confianza del entity
-      console.log(entities);
       var keys = [];
-      if (confint >= 0.6 && confent >= 0.6) {
+      if (confint >= 0.8 && confent >= 0.8) {
         for (var j in respuesta.entities) keys.push(j);
         let entitie = entities[j.split(":")[1]];
         if (entitie === undefined) {
@@ -95,7 +64,11 @@ class ActionProvider {
         }
         const action = traits[respuesta.traits.mensaje_instant_neo[0].value];
         const resultado =
-          intents[respuesta.intents[0].name] + " " + entitie + " " + action;
+          intents.intents[respuesta.intents[0].name] +
+          " " +
+          entitie +
+          " " +
+          action;
         const greetingMessage = this.createChatBotMessage(resultado);
         this.updateChatbotState(greetingMessage);
         if (resultado.includes("página de cátedra")) {
---
+++ src/intents.js
@@ -0,0 +1,34 @@
+export default {
+  intents : {
+    mensajes: "Ponerse en contacto",
+    cambios: "Pagina de cátedra",
+    apuntes:
+      "Es necesario cambiar a FIUBA la institución en tu perfil. Luego",
+    ejercicios: "La Guía de problemas",
+    examenes: "Tanto la constancia de parcial como la de integrador",
+    aula_virtual: "En todos los casos el aula virtual",
+    preguntas: "Es necesario",
+    notas: "se forma",
+    coordinadora:
+      "la coordinación se encarga de la gestión de la asignatura.",
+    mail: "El mail es",
+    cursos: "tenés que ver el link",
+    sp: "Si sos recursantes",
+    informacion: "En la página de cátedra",
+    recursante: "Sos recursante si ",
+    inscripcion_final:
+      "Por una cuestión organizativa sólo podrás inscribirte en una fecha a la vez.",
+    examen_sinpresent:
+      "Si estás inscripto a un final y no venís no perdés una oportunidad.",
+    cert_exam: "Los certificados de examen se bajan del link de más abajo.",
+    ver_siu: "Tenés que verificar tu historia académica en el SIU-Guaraní.",
+    insc_fisica:
+      "No es posible anotarse en ninguna materia del departamento fuera de término.",
+    anot_nocorrel:
+      "Al anotarte en una materia, si no te desinscribís durante el período de desinscripción indicado en la pregunta anterior, perderás la cursada vigente y las chances que te quedan.",
+    hor_dep:
+      "Los horarios de las asginaturas están en la pagina del departamento.",
+    inscripcion:
+      "Te tenés que inscribir dentro del período habilitado a través del SIU."
+  },
+}
